580f1d26905d337fc49f23242d0d857eae6cccee
Architbh007 <bhullararchit@gmail.com>
2026-04-22 17:03:29 +1000

    adding closed loop square file

---
+++ 4.2C/closed_loop_square.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+import rospy
+from duckietown_msgs.msg import Twist2DStamped
+from duckietown_msgs.msg import WheelEncoderStamped
+
+class ClosedLoopSquare:
+    def __init__(self):
+        rospy.init_node('closed_loop_square', anonymous=True)
+
+        self.pub = rospy.Publisher(
+            '/mybota002437/car_cmd_switch_node/cmd',
+            Twist2DStamped,
+            queue_size=1
+        )
+
+        # Encoder values
+        self.left_ticks = 0
+        self.right_ticks = 0
+        self.left_start = 0
+        self.right_start = 0
+
+        # Tuned parameters
+        self.forward_speed = 0.22
+        self.turn_speed = 2.2
+        self.ticks_per_metre = 368
+        self.ticks_per_90deg = 48
+
+        self.rate = rospy.Rate(10)
+        self.msg = Twist2DStamped()
+
+        # Subscribers
+        rospy.Subscriber(
+            '/mybota002437/left_wheel_encoder_node/tick',
+            WheelEncoderStamped,
+            self.left_encoder_callback,
+            queue_size=1
+        )
+        rospy.Subscriber(
+            '/mybota002437/right_wheel_encoder_node/tick',
+            WheelEncoderStamped,
+            self.right_encoder_callback,
+            queue_size=1
+        )
+
+    def left_encoder_callback(self, msg):
+        self.left_ticks = msg.data
+
+    def right_encoder_callback(self, msg):
+        self.right_ticks = msg.data
+
+    def avg_ticks(self):
+        left_diff = abs(self.left_ticks - self.left_start)
+        right_diff = abs(self.right_ticks - self.right_start)
+        return (left_diff + right_diff) / 2.0
+
+    def reset_ticks(self):
+        self.left_start = self.left_ticks
+        self.right_start = self.right_ticks
+
+    def stop_robot(self):
+        for _ in range(10):
+            self.msg.header.stamp = rospy.Time.now()
+            self.msg.v = 0.0
+            self.msg.omega = 0.0
+            self.pub.publish(self.msg)
+            self.rate.sleep()
+
+    def move_straight(self, distance, speed):
+        tick_target = int((distance / 1.0) * self.ticks_per_metre)
+        self.reset_ticks()
+        rospy.loginfo("Moving straight %sm (%d ticks)...", distance, tick_target)
+
+        self.msg.header.stamp = rospy.Time.now()
+        self.msg.v = speed if distance > 0 else -speed
+        self.msg.omega = -0.010
+        self.pub.publish(self.msg)
+
+        while not rospy.is_shutdown() and self.avg_ticks() < tick_target:
+            self.msg.header.stamp = rospy.Time.now()
+            self.msg.omega = -0.010
+            self.pub.publish(self.msg)
+            self.rate.sleep()
+
+        self.stop_robot()
+        rospy.sleep(1)
+
+    def rotate(self, angle_deg, speed):
+        tick_target = int((angle_deg / 90.0) * self.ticks_per_90deg)
+        self.reset_ticks()
+        rospy.loginfo("Rotating %d degrees (%d ticks)...", angle_deg, tick_target)
+
+        self.msg.header.stamp = rospy.Time.now()
+        self.msg.v = 0.0
+        self.msg.omega = speed if angle_deg > 0 else -speed
+        self.pub.publish(self.msg)
+
+        while not rospy.is_shutdown() and self.avg_ticks() < tick_target:
+            self.msg.header.stamp = rospy.Time.now()
+            self.pub.publish(self.msg)
+            self.rate.sleep()
+
+        self.stop_robot()
+        rospy.sleep(1)
+
+    def drive_square(self):
+        rospy.sleep(2)
+        for i in range(4):
+            rospy.loginfo("Starting side %d of square", i + 1)
+            self.move_straight(1.0, self.forward_speed)
+            self.rotate(90, self.turn_speed)
+
+        self.stop_robot()
+        rospy.loginfo("Square complete!")
+
+if __name__ == '__main__':
+    try:
+        driver = ClosedLoopSquare()
+        driver.drive_square()
+    except rospy.ROSInterruptException:
+        pass
